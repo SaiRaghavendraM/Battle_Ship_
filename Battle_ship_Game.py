d3aa07d2754f7364c238091e03d5f820a7a6529e
sai raghavendra <sairaghavendra.mangurai@gmail.com>
2024-11-09 16:01:30 +0530

    added docs

--- Battle_ship_Game.py
+++ Battle_ship_Game.py
@@ -1,24 +1,25 @@
 import random
 
+# Constants
+BOARD_SIZE = 4
 
 def welcome_message():
+    """Prints the welcome message for the game."""
     print("Welcome to Battle Ship")
     print("There's a hidden battleship somewhere on this board.\n"
-          'Enter your row and column guesses to sink it!')
+          "Enter your row and column guesses to sink it!")
 
-
-# Create a square board based on dims value
 def build_board(dims):
+    """Creates a square board based on the given dimensions."""
     return [['O' for _ in range(dims)] for _ in range(dims)]
 
-
 def print_board(board):
+    """Prints the current state of the board."""
     for row in board:
         print(*row)
 
-
-# Create and return ship positional coordinates
 def build_ship(dims):
+    """Creates and returns ship positional coordinates."""
     len_ship = random.randint(2, dims)
     orientation = random.choice(['horizontal', 'vertical'])
     if orientation == 'horizontal':
@@ -31,37 +32,44 @@ def build_ship(dims):
         coords = [(row, col) for row in range(row_start, row_start + len_ship)]
     return coords
 
-def user_guess():
+def user_guess(dims):
+    """Prompts the user for a guess and returns it as a tuple."""
     while True:
         try:
             row = int(input('Row: ')) - 1
             col = int(input('Col: ')) - 1
-            return (row, col)
+            if 0 <= row < dims and 0 <= col < dims:
+                return (row, col)
+            else:
+                print(f"Please enter numbers between 1 and {dims}.")
         except ValueError:
             print("Please enter valid integers for row and column.")
 
 def update_board(guess, board, ship, guesses):
+    """Updates the board based on the user's guess."""
     if guess in guesses:
-        print('Oops! You’ve already guessed there! Trying to trick the ocean, are we?')
+        print("Oops! You’ve already guessed there! Trying to trick the ocean, are we?")
         return board
     guesses.add(guess)
     if guess in ship:
-        print('You hit my battleship!')
+        print("You hit my battleship!")
         board[guess[0]][guess[1]] = 'X'
         ship.remove(guess)
     else:
-        print('LOL miss!')
+        print("LOL miss!")
     return board
 
 def main():
+    """Main function to run the Battleship game."""
     welcome_message()
-    dims = 4
-    board = build_board(dims)
-    ship = build_ship(dims)
+    board = build_board(BOARD_SIZE)
+    ship = build_ship(BOARD_SIZE)
     guesses = set()
     while ship:
         print_board(board)
-        guess = user_guess()
+        guess = user_guess(BOARD_SIZE)
         board = update_board(guess, board, ship, guesses)
+    print("Congratulations! You've sunk the battleship!")
 
-main()
+if __name__ == "__main__":
+    main()
